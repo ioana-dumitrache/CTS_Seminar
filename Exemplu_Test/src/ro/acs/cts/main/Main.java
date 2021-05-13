bad947989408c04a58295dd1dde9fbf0e058b398
ioana-dumitrache <yoanna_dumitrache@yahoo.com>
2021-05-13 09:29:55 +0300

    after break seminar11 main

--- Exemplu_Test/src/ro/acs/cts/main/Main.java
+++ Exemplu_Test/src/ro/acs/cts/main/Main.java
@@ -1,10 +1,26 @@
 package ro.acs.cts.main;
 
+import ro.ase.cts.adapter.AdapterCont;
+import ro.ase.cts.factory.ContBancar;
+import ro.ase.cts.singleton.SingletonBanca;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SingletonBanca banca = SingletonBanca.getInstance("BCR", "Romana");
+		System.out.println(SingletonBanca.NRCONTURI);
+		
+		ContBancar contBancar1 = banca.createInstance("CREDIT");
+		System.out.println(SingletonBanca.NRCONTURI);
+		
+		ContBancar contBancar2 = banca.createInstance("DEBIT");
+		System.out.println(SingletonBanca.NRCONTURI);
+		
+		ContBancar adapterCont = new AdapterCont();
+		
+		adapterCont.transfer(contBancar1, 20);
+		adapterCont.depunde(30);
 	}
 
 }
